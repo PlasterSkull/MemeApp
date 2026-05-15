5e262228822fc7faae12eacbcf41f879a37b5e28
Ivan Stuk <ivan.stuk@inbox.ru>
2026-05-15 17:30:29 +0300

    feat(common): add LinqExt.ForEach for IEnumerable<T>

---
+++ src/dotnet/Shared/MemeApp.Common/Extensions/LinqExt.cs
@@ -0,0 +1,10 @@
+namespace MemeApp.Common.Extensions;
+
+public static class LinqExt
+{
+    public static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
+    {
+        foreach (var item in source)
+            action(item);
+    }
+}
